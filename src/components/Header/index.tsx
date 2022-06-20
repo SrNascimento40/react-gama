1d222be2331698b9c2a7282409eb1a8f08f014cc
Nascimento <wallacediogo10@hotmail.com>
2022-06-20 16:26:52 -0300

    Props

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,18 @@
+import Link from "../Link"
+
 import "./styles.css";
 
 function Header() {
     return (
     <header>
-        <h1>Header</h1>
-    </header>);
+        <p>123 Bitcoin</p>
+        <nav>
+            <Link text="Home" redirect="/home"/>
+            <Link text="Marketplace" redirect="/marketplace"/>
+            <Link text="Contact" redirect="/contact"/>
+        </nav>
+    </header>
+    );
 }
 
-export default Header
+export default Header;
---
+++ src/components/Link/index.tsx
@@ -0,0 +1,16 @@
+import "./styles.css"
+
+interface linkProps{
+    text:string;
+    redirect:string;
+}
+
+function Link (props: linkProps) {
+    return (
+        <a href={props.redirect} target="_blank" rel="noreferrer">
+            {props.text}
+        </a>
+    )
+}
+
+export default Link
