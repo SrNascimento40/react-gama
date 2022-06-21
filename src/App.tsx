d3bb72fe1559dc672defd26dd5a288a433f44dcc
Nascimento <wallacediogo10@hotmail.com>
2022-06-21 11:58:37 -0300

    created box and learned about events

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import Header from "./components/Header";
+import Box from "./components/Box"
+
 import "./styles/global.css"
+
+const testandoAlert = () => {
+  alert('alerta teste');
+}
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <div>
+          <button className="button" onClick={testandoAlert}>Clique aqui para clicar aqui</button>
+        </div>
+        <Box background="claro">
+          <h1>
+            teste, um, dois... testando
+          </h1>
+        </Box>
+        <Box background="escuro">
+          <p>
+            teste 2...
+          </p>
+        </Box>
+      </main>
     </div>
   );
 }
--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,9 +1,10 @@
-import "style.css"
+import "./style.css"
 
 interface boxProps {
     background: string;
+    children: JSX.Element;
 }
 
 export default function Box (props: boxProps) {
-    return <section className="box"></section>
+    return <section className={`box ${props.background}`}>{props.children}</section>
 }
